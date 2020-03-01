3c3c94ecf4e2489add2929d53c47daa6b123e040
pitersikora <piootrsikora@gmail.com>
2020-03-01 06:55:45 +0100

    [woly_newtona] comment added, print format changes

--- Zadania/23_02_2020/woly_newtona.py
+++ Zadania/23_02_2020/woly_newtona.py
@@ -1,15 +1,18 @@
+# Program do symulacji "Problemu Wolow Newtona" - w parametrach startowych programu nalezy podac
+# liczbe morgow pola dostepnego do wypasu oraz liczbe tygodni przez jaki odbywa sie wypas.
+
 import sys
 import math
 
 def input_get():
-    print "Program symulujacy problem \"Wolow Newtona\"\n"
+    print 'Program symulujacy problem \"Wolow Newtona\"\n'
     try:
         if sys.argv[1].isdigit() == False or sys.argv[2].isdigit() == False:
-            print "\033[91mPodana wartosc nie jest liczba !!!\033[00m"
+            print '\033[91mPodana wartosc nie jest liczba !!!\033[00m'
             exit(2)
     except Exception as exc:
         print exc
-        print "\033[91mPodaj liczbe morgow oraz czas w parametrach startowych programu !!!\033[92m"
+        print '\033[91mPodaj liczbe morgow oraz liczbe tygodni w parametrach startowych programu !!!\033[92m'
         exit(1)
     print "Liczba morgow do wypasu: %s\nLiczba tygodni: %s\n" % (sys.argv[1], sys.argv[2])
     return int(sys.argv[1]), int(sys.argv[2])
@@ -35,9 +38,9 @@ def main():
     print "W ciagu %s tygodni urosna dodatkowe jednostki trawy w ilosci: %d" % (grow_time, field * grow_time)
     print_grass(field * grow_time)
     print "\nLaczna ilosc trawy do wypasu to : \033[92m%d\033[00m\n" % (field * 4 + field * grow_time)
-    print "Jedna krowa zjada 2 jednostki trawy tygodniowo\n"
+    print 'Jeden wol zjada 2 jednostki trawy tygodniowo\n'
     calc =int(math.ceil(((field * 4.0) + (field * grow_time)) / (grow_time * 2.0)))
-    print "Do pelnego wypasu potrzeba krow w ilosci: \033[95m%d\033[00m" % calc
+    print "Do pelnego wypasu potrzeba wolow w ilosci: \033[95m%d\033[00m" % calc
     print_cow(calc)
 
 if __name__ == "__main__":
