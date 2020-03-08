22a2ce17dbd3835583bc2cbcf5fe9422f70b37a7
pitersikora <piootrsikora@gmail.com>
2020-03-08 16:16:06 +0100

    [rycerze] code changed for better testing

--- Zadania/23_02_2020/rycerze.py
+++ Zadania/23_02_2020/rycerze.py
@@ -2,39 +2,61 @@
 # nalezy podac liczbe rycerzy bioracych udzial w symulacji.
 
 import sys
-print 'Program symulujacy problem Jozefa Flawiusza\n'
-try:
-    knights = sys.argv[1]
-    if knights.isdigit() == False:
-        print '\033[91mPodana wartosc nie jest liczba !!!\033[00m'
-        exit(2)
-except Exception as exc:
-    print exc
-    print '\033[91mPodaj liczbe rycerzy w parametrach startowych programu !!!\033[00m'
-    exit(1)
-print "Liczba rycerzy: %s" % knights
 
-death_ring = []
-for i in range(0, int(knights)):
-    death_ring.append(i+1)
+def get_input(user_input):
+    print ('Program symulujacy problem Jozefa Flawiusza\n')
+    try:
+        if user_input.isdigit() == False:
+            print ('\033[91mPodana wartosc nie jest liczba !!!\033[00m')
+            exit(2)
+    except Exception as exc:
+        print (exc)
+        print ('\033[91mPodaj liczbe rycerzy w parametrach startowych programu !!!\033[00m')
+        exit(1)
+    print ("Liczba rycerzy: %s" % user_input)
+    return int(user_input)
 
-round_counter = 1
+def generate_knights(knights):
+    generated_ring = []
+    if knights == None:
+        print ('\033[91mNiewlasciwy argument funkcji!!!\033[00m')
+        exit(3)
+    for i in range(0, int(knights)):
+        generated_ring.append(i+1)
+    return generated_ring
 
-while len(death_ring) != 1:
-    round_string = ""
-    temporary_list = []
-    print "\nRunda: %s\n" % round_counter
-    for i in range(0, len(death_ring)):
-        if i % 2 == 0:
-            round_string += str(death_ring[i]) + " "
-            temporary_list.append(death_ring[i])
-        else:
-            round_string += "\033[1;31;49m%s \033[00m" % str(death_ring[i])
-    print round_string
-    if len(death_ring) % 2 == 1:
-        temporary_list.insert(0, temporary_list[-1])
-        del temporary_list[-1]
-    death_ring = temporary_list
-    round_counter += 1
+def simulate_problem(array_to_simulate):
+    if array_to_simulate == None:
+        print ('\033[91mNiewlasciwy argument funkcji!!!\033[00m')
+        exit(3)
+    round_counter = 1
+    while len(array_to_simulate) != 1:
+        round_string = ""
+        temporary_list = []
+        print ("\nRunda: %s\n" % round_counter)
+        for i in range(0, len(array_to_simulate)):
+            if i % 2 == 0:
+                round_string += str(array_to_simulate[i]) + " "
+                temporary_list.append(array_to_simulate[i])
+            else:
+                round_string += "\033[1;31;49m%s \033[00m" % str(array_to_simulate[i])
+        print (round_string)
+        if len(array_to_simulate) % 2 == 1:
+            temporary_list.insert(0, temporary_list[-1])
+            del temporary_list[-1]
+        array_to_simulate = temporary_list
+        round_counter += 1
+    return array_to_simulate
 
-print "\nW kregu smierci zlozonym z %s rycerzy, lepiej zebys stal na \033[92m%d\033[00m miejscu !!!" % (knights, death_ring[0])
+def print_result(array_to_fill, knights):
+    print ("\nW kregu smierci zlozonym z %s rycerzy, lepiej zebys stal na \033[92m%d\033[00m miejscu !!!" % (knights, array_to_fill[0]))
+
+def main():
+    knights_ammount = get_input(sys.argv[1])
+    print (knights_ammount)
+    death_ring = generate_knights(knights_ammount)
+    death_ring = simulate_problem(death_ring)
+    print_result(death_ring, knights_ammount)
+
+if __name__ == "__main__":
+    main()
