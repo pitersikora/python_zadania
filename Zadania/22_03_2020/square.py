7db4baf4ae89402360671fdfe204ac31196c990e
pitersikora <piootrsikora@gmail.com>
2020-03-22 14:50:55 +0100

    [square] program for testing if number is square

---
+++ Zadania/22_03_2020/square.py
@@ -0,0 +1,11 @@
+# Square.py
+
+import math
+
+number = input("Podaj liczbe\n")
+number = math.sqrt(int(number))
+
+if number.is_integer():
+    print('Liczba jest kwadaratem innej liczby, ta liczba to %d' % number)
+else:
+    print('Liczba nie jest kwadratem innej liczby')
