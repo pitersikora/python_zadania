f05fcb2a446271b3eeef3a733cfe0daeab2c9312
pitersikora <piootrsikora@gmail.com>
2020-03-23 16:28:03 +0100

    [square] program rewritten using basic calculus

--- Zadania/22_03_2020/square.py
+++ Zadania/22_03_2020/square.py
@@ -6,6 +6,6 @@ number = input("Podaj liczbe\n")
 number = math.sqrt(int(number))
 
 if number.is_integer():
-    print('Liczba jest kwadaratem innej liczby, ta liczba to %d' % number)
+    print('Liczba jest kwadratem innej liczby, ta liczba to %d' % number)
 else:
     print('Liczba nie jest kwadratem innej liczby')
