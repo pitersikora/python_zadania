787798cadaa5f64065d1f417c2441bb4bf30852d
pitersikora <52927920+pitersikora@users.noreply.github.com>
2020-04-05 15:00:54 +0200

    Update equation_simple.py

--- Zadania/01_03_2020/equation_simple.py
+++ Zadania/01_03_2020/equation_simple.py
@@ -2,19 +2,22 @@
 import math
 
 def pierwiaski_trojmianu(a, b, c):
+    if b == 0:
+        if c == 0:
+            return {"ile_pierwiastkow": -1, "pierwiastki": "Nieskonczone"}
+        else:
+            return {"ile_pierwiastkow": 1 , "pierwiastki": (-1 * c / b)}
     delta = b**2 - (4 * a * c)
+    
     if delta < 0:
         return {"ile_pierwiastkow": 0, "pierwiastki": None}
-        exit(0)
     elif delta == 0:
         return {"ile_pierwiastkow": 1, "pierwiastki": (-1 * b / (2 * a))}
-        exit(0)
     else:
         result_1 = (-1 * b - math.sqrt(delta)) / (2 * a)
         result_2 = (-1 * b + math.sqrt(delta)) / (2 * a)
         return {"ile_pierwiastkow": 2 , "pierwiastki": (result_1, result_2)}
-        exit(0)
 
-#print(pierwiaski_trojmianu(2, 3, 2))
+#print(pierwiaski_trojmianu(2, 0, 0))
 #print(pierwiaski_trojmianu(2, 2, -12))
 #print(pierwiaski_trojmianu(2, 4, 2))
